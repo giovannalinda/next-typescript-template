0b1f88770c855b7cc169b350e0aca3d6b0e2b958
Giovanna Linda <eugiovannasouza@gmail.com>
2022-08-24 12:08:49 -0300

    feat: setup storybook

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, Story } from '@storybook/react'
+import { Button } from './Button'
+
+export default {
+  title: 'Button',
+  component: Button,
+} as Meta
+
+export const Default: Story = () => <Button />
